3c0c03af60fb66233d211ab88e9db20ad44bf541
Tim Buchinger <timbuchinger@gmail.com>
2025-11-24 23:32:23 -0500

    chore: relax merge ignore

--- commitlint.config.js
+++ commitlint.config.js
@@ -3,7 +3,7 @@ const normalize = (message) => message.trim();
 module.exports = {
   extends: ["@commitlint/config-conventional"],
   ignores: [
-    (message) => normalize(message).startsWith("Merge branch "),
+    (message) => normalize(message).startsWith("Merge "),
     (message) => normalize(message) === "Initial plan",
   ],
 };
